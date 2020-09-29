65000b1222d5d797f29f58d5fbe8ba1a4ae07974
cool-conall <37257817+cool-conall@users.noreply.github.com>
2020-09-30 08:27:42 +1000

    Fixed I code, for you.

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
+const about = document.getElementById('about')
+const services = document.getElementById('services')
+const xp = document.getElementById('xp')
 
-
-hamburger.addEventListener('click', () =>{
-    navLinks.classList.toggle('open');
-     
+about.addEventListener('mouseover', () =>{
+    about.style.color = "blue"
+    console.log('Event triggered')
+})
+services.addEventListener('mouseover', () =>{
+    services.style.color = "blue"
+    console.log('Event triggered')
+})
+xp.addEventListener('mouseover', () =>{
+    xp.style.color = "blue"
+    console.log('Event triggered')
 })
